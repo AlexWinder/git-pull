fa611237a528a3e4c4624ff40fea60740de104aa
Alex Winder <contact@alexwinder.uk>
2022-04-29 15:55:26 +0100

    Improvements to git-pull script

--- git-pull.sh
+++ git-pull.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+# Variables for colouring text
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Colour
+
 # Location of where the Git repositories are located
 pwd=$(pwd)
 directory="${directory:-$pwd}"
+
+# Default blurb
+echo
+echo -e "${YELLOW}Git Pull Automation Script${NC}"
+echo
+echo "This script cycles through a directory and then runs a 'git pull' against any Git projects."
+echo "https://github.com/AlexWinder/git-pull"
+echo
 
 # Enable named parameters to be passed in
 while [ $# -gt 0 ]; do
     # If help has been requested
     if [[ $1 == *"--help" ]]; then
-        echo
-        echo "Git Pull Automation Script"
-        echo
-        echo "This script cycles through a directory and then runs a 'git pull' against any Git projects."
-        echo "https://github.com/AlexWinder/git-pull"
-        echo
         echo "Options:"
         echo
         echo "--directory   The location where your Git projects are located. Default: current working directory"
@@ -34,11 +42,13 @@ done
 for i in $(find $directory -maxdepth 1 -mindepth 1 -type d); do
     # Navigate to the directory
     cd $i
-    pwd
+    echo -e "${GREEN}${i}${NC}"
+    echo
 
     # Check if the project is Git
     if [ ! -d ".git" ]; then
-        echo "Not a Git project. Skipping."
+        echo -e "${YELLOW}Not a Git project. Skipping.${NC}"
+        echo
         continue
     fi
 
@@ -46,18 +56,43 @@ for i in $(find $directory -maxdepth 1 -mindepth 1 -type d); do
     remotes=$(git remote show)
     remotes_size=${#remotes}
     if (( ! remotes_size > 0 )); then
-        echo "Git project has no remote. Skipping."
+        echo -e "${YELLOW}Git project has no remote. Skipping.${NC}"
+        echo
         continue
     fi
 
-    # Do a pull on the project
-    git fetch --all
-    for branch in "$@"; do
-        git checkout "$branch"  || exit 1
-        git pull                || exit 1
+    # Get the default origin
+    default_branch=$(git remote show ${remotes[0]} | sed -n '/HEAD branch/s/.*: //p')
+    echo "Checkout to default branch..."
+    git checkout $default_branch
+    # Fetch everything
+    echo "Fetch everything..."
+    git fetch --all --prune
+    # Pull everything
+    echo "Pull everything..."
+    git pull --all --ff-only
+    # Set up a tracked branch for each branch
+    echo "Setting up a tracked branch for each branch..."
+    git branch -r | grep -v '\->' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | while read remote; do
+        echo "Tracking $remote"
+        git branch --track "${remote#origin/}" "$remote"
     done
+    # Do a pull on each branch
+    echo "Pull each tracked branch..."
+    git branch --format "%(refname:short)" | while read branch; do
+        echo "Pull ${remotes[0]} $branch"
+        git checkout $branch
+        git pull --ff-only
+    done
+    # Delete any branches which haven't been merged into the default
+    echo "Remove deleted branches"
+    git fetch -p && git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -d
+    # Back to default branch
+    echo "Reverting back to the default branch"
+    git checkout $default_branch
+    echo
 done
 
-echo "All projects pulled."
+echo -e "${GREEN}All projects pulled.${NC}"
 
 exit 0
